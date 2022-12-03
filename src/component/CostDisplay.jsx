75852059d8f38e3a8617bb7a798b4877c263feef
Nue <phonehtetaung1993@gmail.com>
2022-12-03 13:39:31 +0630

    fixing the cost display

--- src/component/CostDisplay.jsx
+++ src/component/CostDisplay.jsx
@@ -13,14 +13,14 @@ export default function CostDisplay({resetChange}) {
                 <p>Tip Amount</p>
                 <h4 className=' text-neutral-Dark-grayish-cyan'>/ person</h4>
             </div>
-            <h1 className=' text-primary-Strong-cyan'>{tipAmount ? `$${tipAmount.toFixed(2)}` : `0.00`}</h1>
+            <h1 className=' text-primary-Strong-cyan'>{tipAmount ? `$${tipAmount.toFixed(2)}` : `$0.00`}</h1>
         </div>
         <div className=' my-5 flex items-center justify-between'>
             <div>
                 <p>Total</p>
                 <h4 className=' text-neutral-Dark-grayish-cyan'>/ person</h4>
             </div>
-            <h1 className=' text-primary-Strong-cyan'>{total ? `$${total.toFixed(2)}` : `0.00`}</h1>
+            <h1 className=' text-primary-Strong-cyan'>{total ? `$${total.toFixed(2)}` : `$0.00`}</h1>
         </div>
         <button 
             className=' hover:bg-neutral-Light-grayish-cyan lg:mt-32 text-xl rounded-md w-full text-center text-neutral-Very-dark-cyan bg-primary-Strong-cyan py-2'
